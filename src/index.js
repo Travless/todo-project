8842d9dad81b56949f8a47c1a5c8c738ce61c19d
Travless <travis.white0101@gmail.com>
2022-12-10 14:21:36 -0500

    address event listener issue where id and complete status of new todos is not being assigned or viewable

--- src/index.js
+++ src/index.js
@@ -4,55 +4,131 @@ import { Project, ToDo } from './classes';
 const projectFormInput = document.querySelector('[data-project-form-input]');
 const projectFormAdd = document.querySelector('[data-project-form-add]');
 const projectCurrent = document.querySelector('[data-current-projects]');
-// const projectMenuItem = document.querySelector('[data-project-menu-item]');
+const toDoCont = document.getElementById('todo-cont');
 const toDoHeaderTitle = document.querySelector('[data-todo-header-title]');
 const toDosRemaining = document.querySelector('[data-todos-remaining]');
 const toDosCurrent = document.querySelector('[data-current-todos]');
 const toDoFormInput = document.querySelector('[data-todo-form-input]');
-const toDoFormAdd = document.querySelector('[data-todo-form-input]');
+const toDoFormAdd = document.querySelector('[data-todo-form-add]');
 const toDoRemoveFinishedBtn = document.querySelector('[data-remove-finished-todos]');
 const projectRemoveBtn = document.querySelector('[data-remove-project]');
+const toDoTemplate = document.getElementById('todo-template');
 
 const LOCAL_STORAGE_PROJECT_KEY = 'todo.projects';
 const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'todo.selectedProjectId';
 
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [];
-// let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY);
+let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY);
+
+
+projectCurrent.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li'){
+        selectedProjectId = e.target.dataset.projectId;
+        saveAndLoad();
+    }
+})
+
+toDosCurrent.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'input'){
+        const selectedProject = projects.find(project => project.id === selectedProjectId);
+        const selectedToDo = selectedProject.toDos.find(toDo => toDo.id === e.target.id);
+        selectedToDo.complete = e.target.checked;
+        save()
+    }
+})
 
 projectFormAdd.addEventListener('click', e => {
     e.preventDefault()
 
     const projName = projectFormInput.value;
-    if(projName == nul || projName === ''){
+    if(projName == null || projName === ''){
         return;
     }
     const newProj = new Project(projName);
     projectFormInput.value = null;
     projects.push(newProj);
     console.log(projects);
-    save();
-    render();
+    saveAndLoad();
 });
 
-projectCurrent.addEventListener('click', e => {
-    toDoHeaderTitle.innerText = `${projMenu.innerText}`;
+
+toDoFormAdd.addEventListener('click', e => {
+    e.preventDefault();
+
+    let toDoTitle = toDoFormInput.value;
+    if(toDoTitle == null || toDoTitle === ''){
+        return;
+    }
+    const newToDo = new ToDo(toDoTitle);
+    toDoFormInput.value = null;
+    const selectedProject = projects.find(project => project.id === selectedProjectId);
+    selectedProject.toDos.push(newToDo);
+    saveAndLoad();
+    console.log(selectedProject);
 })
 
-const render = () => {
+toDoRemoveFinishedBtn.addEventListener('click', e => {
+    const selectedProject = projects.find(project => project.id === selectedProjectId);
+    selectedProject.toDos = selectedProject.toDos.filter(toDo => !toDo.complete);
+    saveAndLoad();
+})
+
+projectRemoveBtn.addEventListener('click', e => {
+    projects = projects.filter(list => list.id !== selectedProjectId);
+    selectedProjectId = null;
+    saveAndLoad();
+})
+
+const saveAndLoad = () => {
+    save();
+    load();
+}
+
+const save = () => {
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId);
+};
+
+const load = () => {
     clearElements(projectCurrent);
+    loadProjects();
+    let selectedProject = projects.find(project => project.id === selectedProjectId);
+    if (selectedProjectId == null){
+        toDoCont.style.display = 'none';
+    } else {
+        toDoCont.style.display = '';
+        toDoHeaderTitle.innerText = selectedProject.name;
+        clearElements(toDosCurrent);
+        loadToDo(selectedProject);
+    }
+
+}
+
+const loadProjects = () => {
     projects.forEach(project => {
         let projMenu = document.createElement('li');
         projMenu.dataset.projectId = project.id;
         projMenu.classList.add('project-menu-item');
         projMenu.innerText = project.name;
+        if(project.id === selectedProjectId){
+            projMenu.classList.add('selected-project');
+        };
         projectCurrent.append(projMenu);
     })
 }
 
-const save = () => {
-    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects));
-    // localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId);
-};
+const loadToDo = (selectedProject) => {
+    selectedProject.toDos.forEach(toDo => {
+        const toDoElement = document.importNode(toDoTemplate.content, true);
+        const checkbox = document.querySelector('[data-todo-checkbox]')
+        checkbox.id = toDo.id;
+        checkbox.checked = toDo.complete;
+        const label = toDoElement.querySelector('label');
+        label.htmlFor = toDo.id;
+        label.append(toDo.name);
+        toDosCurrent.appendChild(toDoElement);
+    })
+}
 
 const clearElements = (element) => {
     while(element.firstChild){
@@ -60,4 +136,4 @@ const clearElements = (element) => {
     }
 }
 
-render();
+load();
